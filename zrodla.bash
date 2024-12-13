7ad9ed0bde46b41daa1da5e38f6634c3f5991dd6
Tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-12-13 17:48:35 +0100

    wykonywanie testow

--- zrodla.bash
+++ zrodla.bash
@@ -1,6 +1,6 @@
 BUILD_DIR="build"
-TESTS_DIR="test/USA-road-t"
-RESULTS_DIR="wyniki/USA-road-t"
+TESTS_DIR="test/Random4-C-5w"
+RESULTS_DIR="wyniki/Random4-C-5w"
 
 for program in "$BUILD_DIR"/*; do
 
@@ -10,7 +10,9 @@ for program in "$BUILD_DIR"/*; do
     test_name=$(basename "$test_file" .gr)
     source_file="${test_file%.*}.ss"
     result_file="$RESULTS_DIR"/"$program_name"-"$test_name".ss.res
-
     "$program" -d "$test_file" -ss "$source_file" -oss "$result_file"
+    #source_file="${test_file%.*}.p2p"
+    #result_file="$RESULTS_DIR"/"$program_name"-"$test_name".p2p.res
+    #"$program" -d "$test_file" -p2p "$source_file" -op2p "$result_file"
   done
 done
